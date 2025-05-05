7046c74aa2211f0ef70080681d1d12a2c6488447
skuerjuan <maierowiczfelipe@gmail.com>
2025-05-05 10:06:29 -0300

    EAC Terminado

---
+++ Assets/ClaveManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ClaveManager : MonoBehaviour
+{
+    string clave = "holajero";
+    public TextMeshProUGUI txtResultado;
+    public TMP_InputField inputClave;
+    string claveIngresada;
+    string mensajeFinal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtResultado.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Autenticar()
+    {
+        claveIngresada = inputClave.text;
+        if (claveIngresada == clave)
+        {
+            mensajeFinal = "Clave Correcta, Bienvenido!";
+        }
+        else
+        {
+            mensajeFinal = "Clave Incorrecta.";
+        }
+        txtResultado.text = mensajeFinal; 
+    }
+}
